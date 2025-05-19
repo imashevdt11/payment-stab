a5a2377036c41538eacde11b24df8495dc9bf8f2
imashevdt11 <imashevdt@gmail.com>
2025-05-19 16:22:19 +0600

    add openapi docs

--- src/main/java/com/example/payment_stab/controller/PaymentController.java
+++ src/main/java/com/example/payment_stab/controller/PaymentController.java
@@ -5,6 +5,9 @@ import com.example.payment_stab.model.Card;
 import com.example.payment_stab.model.PaymentRequest;
 import com.example.payment_stab.model.TopUpRequest;
 import com.example.payment_stab.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/payments")
 public class PaymentController {
@@ -23,21 +27,25 @@ public class PaymentController {
         this.service = service;
     }
 
+    @Operation(summary = "Оплата")
     @PostMapping("/process")
-    public String processPayment(@RequestBody PaymentRequest request) {
+    public String processPayment(@RequestBody @Valid PaymentRequest request) {
         return service.processPayment(request);
     }
 
+    @Operation(summary = "Добавление карты")
     @PostMapping("/cards/add")
-    public String addCard(@RequestBody AddCardRequest request) {
+    public String addCard(@RequestBody @Valid AddCardRequest request) {
         return service.addCard(request);
     }
 
+    @Operation(summary = "Пополнение баланса")
     @PostMapping("/top-up")
-    public String topUpBalance(@RequestBody TopUpRequest request) {
+    public String topUpBalance(@RequestBody @Valid TopUpRequest request) {
         return service.topUpBalance(request);
     }
 
+    @Operation(summary = "Получение информации о всех картах")
     @GetMapping("/cards")
     public List<Card> getAllCards() {
         return service.getAllCards();
